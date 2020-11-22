11b68300fe059721b1a62dc96d7ec74eeecca491
Arkadiusz Osowski <arkos@7n.com>
2020-11-22 17:45:52 +0100

    add Display attr for ResponsibePartyId to  display firendly name of property

--- Todo/Models/TodoItems/TodoItemEditFields.cs
+++ Todo/Models/TodoItems/TodoItemEditFields.cs
@@ -2,26 +2,20 @@
 
 namespace Todo.Models.TodoItems
 {
-    public class TodoItemEditFields
-    {
-        public int TodoListId { get; set; }
-        public string Title { get; set; }
-        public string TodoListTitle { get; set; }
+    public class TodoItemEditFields: TodoItemCreateFields
+    {       
         public int TodoItemId { get; set; }
         public bool IsDone { get; set; }
-        public string ResponsiblePartyId { get; set; }
-        public Importance Importance { get; set; }
+       
 
         public TodoItemEditFields() { }
 
         public TodoItemEditFields(int todoListId, string todoListTitle, int todoItemId, string title, bool isDone, string responsiblePartyId, Importance importance)
-        {
-            TodoListId = todoListId;
-            TodoListTitle = todoListTitle;
+            :base(todoListId, todoListTitle, responsiblePartyId)
+        {          
             TodoItemId = todoItemId;
             Title = title;
-            IsDone = isDone;
-            ResponsiblePartyId = responsiblePartyId;
+            IsDone = isDone;          
             Importance = importance;
         }
     }

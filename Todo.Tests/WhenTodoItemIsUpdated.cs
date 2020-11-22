b2575762db91dc6a3d36e79a80493fc7185c290b
Arkadiusz Osowski <arkos@7n.com>
2020-11-22 18:50:41 +0100

    allow edit item Rank

---
+++ Todo.Tests/WhenTodoItemIsUpdated.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Todo.Data.Entities;
+using Todo.EntityModelMappers.TodoItems;
+using Todo.Models.TodoItems;
+using Xunit;
+
+namespace Todo.Tests
+{
+    public class WhenTodoItemIsUpdated
+    {
+        private readonly TodoItem resultItem;
+        private readonly TodoItemEditFields srcFields;
+
+        public WhenTodoItemIsUpdated()
+        {
+            var todoList = new TestTodoListBuilder(new IdentityUser("alice@example.com"), "shopping")
+                    .WithItem("bread", Importance.High, 0)
+                    .Build()
+                ;
+
+            resultItem = todoList.Items.First();
+            srcFields = TodoItemEditFieldsFactory.Create(resultItem);
+
+            srcFields.Title = "beer";
+            srcFields.IsDone = true;
+            srcFields.ResponsiblePartyId = "simpson.homer@example.com";
+            srcFields.Importance = Importance.Low;
+            srcFields.Rank = 100;
+
+            TodoItemEditFieldsFactory.Update(srcFields, resultItem);
+            
+        }
+
+        [Fact]
+        public void EqualTitle()
+        {
+            Assert.Equal(srcFields.Title, resultItem.Title);
+        }
+
+        [Fact]
+        public void EqualIsDone()
+        {
+            Assert.Equal(srcFields.IsDone, resultItem.IsDone);
+        }
+
+        [Fact]
+        public void EqualResponsiblePartyId()
+        {
+            Assert.Equal(srcFields.ResponsiblePartyId, resultItem.ResponsiblePartyId);
+        }
+
+        [Fact]
+        public void EqualImportance()
+        {
+            Assert.Equal(srcFields.Importance, resultItem.Importance);
+        }
+
+        [Fact]
+        public void EqualRank()
+        {
+            Assert.Equal(srcFields.Rank, resultItem.Rank);
+        }
+    }
+}
--- Todo/Models/TodoItems/TodoItemEditFields.cs
+++ Todo/Models/TodoItems/TodoItemEditFields.cs
@@ -2,21 +2,23 @@
 
 namespace Todo.Models.TodoItems
 {
-    public class TodoItemEditFields: TodoItemCreateFields
-    {       
+    public class TodoItemEditFields : TodoItemCreateFields
+    {
         public int TodoItemId { get; set; }
         public bool IsDone { get; set; }
-       
+        public int Rank { get; set; }
+
 
         public TodoItemEditFields() { }
 
-        public TodoItemEditFields(int todoListId, string todoListTitle, int todoItemId, string title, bool isDone, string responsiblePartyId, Importance importance)
-            :base(todoListId, todoListTitle, responsiblePartyId)
-        {          
+        public TodoItemEditFields(int todoListId, string todoListTitle, int todoItemId, string title, bool isDone, string responsiblePartyId, Importance importance, int rank)
+            : base(todoListId, todoListTitle, responsiblePartyId)
+        {
             TodoItemId = todoItemId;
             Title = title;
-            IsDone = isDone;          
+            IsDone = isDone;
             Importance = importance;
+            Rank = rank;
         }
     }
 }

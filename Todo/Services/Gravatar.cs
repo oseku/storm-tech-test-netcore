779a7482b6574bcbbd7b4c7af68b96f7553e8c7f
Arkadiusz Osowski <arkos@7n.com>
2020-11-22 19:17:04 +0100

    displat gravatar profile info if possible

--- Todo/Services/Gravatar.cs
+++ Todo/Services/Gravatar.cs
@@ -1,10 +1,29 @@
-﻿using System.Security.Cryptography;
+﻿using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Todo.Services
 {
-    public static class Gravatar
+    public class Gravatar
     {
+        private readonly HttpClient _http;
+
+        public Gravatar(HttpClient httpClient)
+        {
+            _http = httpClient;
+            _http.DefaultRequestHeaders.Add("User-Agent", "Todo lsit"); //gravatar returns 403 without User-Agent...
+        }
+
+        public async Task<dynamic> GetProfile(string hash)
+        {
+            var response = await _http.GetAsync($"https://www.gravatar.com/{hash}.json");
+
+            return response.IsSuccessStatusCode
+                ? await response.Content.ReadAsAsync<dynamic>()
+                : null;
+        }
+
         public static string GetHash(string emailAddress)
         {
             using (var md5 = MD5.Create())
--- Todo/Startup.cs
+++ Todo/Startup.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Todo.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Todo.Data;
+using Todo.Services;
 
 namespace Todo
 {
@@ -39,6 +35,8 @@ namespace Todo
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddHttpClient<Gravatar>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
